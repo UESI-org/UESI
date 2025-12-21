7118d4711cd6f59778c9405b889e318d80d998be
0x160000 <openbsd.gnu@gmail.com>
2025-12-21 20:15:03 +0100

    Proper argc/argv parsing

--- user/src/test.c
+++ user/src/test.c
@@ -1,8 +1,25 @@
 #include <syscall.h>
 
 int
-main(void)
+main(int argc, char* argv[])
 {
-	write(1, "Hello, Userland!\n", 17);
-	return 0;
+    char buffer[128];
+    int64_t bytes_read;
+    
+    write(1, "Enter some text: ", 17);
+    bytes_read = read(0, buffer, sizeof(buffer) - 1);
+    
+    if (bytes_read > 0) {
+        buffer[bytes_read] = '\0';
+        
+        write(1, "You entered: ", 13);
+        write(1, buffer, bytes_read);
+        write(1, "\n", 1);
+    } else if (bytes_read == 0) {
+        write(1, "EOF reached\n", 12);
+    } else {
+        write(1, "Read error\n", 11);
+    }
+    
+    return 0;
 }
