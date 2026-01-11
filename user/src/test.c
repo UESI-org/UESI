7b0220dc24c1ee066f7d3024c010aa83c466f5e2
0x160000 <openbsd.gnu@gmail.com>
2026-01-11 16:36:28 +0100

    port: openbsd: assert.h

--- user/src/test.c
+++ user/src/test.c
@@ -1,34 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(void) {
-    int count;
-    int *arr;
-    int sum = 0;
-
-    printf("How many numbers? ");
-    scanf("%d", &count);
-
-    if (count <= 0) {
-        printf("Invalid count\n");
-        return 1;
-    }
-
-    arr = malloc(sizeof(int) * count);
-    if (arr == NULL) {
-        printf("malloc failed\n");
-        return 1;
-    }
-
-    for (int i = 0; i < count; i++) {
-        printf("Number %d: ", i + 1);
-        scanf(" %d", &arr[i]);
-        sum += arr[i];
-    }
-
-    printf("Sum: %d\n", sum);
-    printf("Average: %d\n", sum / count);
-
-    free(arr);
+int
+main() {
+    printf("Hello, World!\n");
     return 0;
 }
